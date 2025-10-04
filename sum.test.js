b1d40b4d56dd4edddb25f397d86f4c88fdc4af6e
Diogo Ferreira <diogo.ferreira18@ua.pt>
2025-10-04 16:58:43 +0100

    Added Pipeline

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,9 @@ const sum = require('./sum');
 test("testar a soma", () => {
     expect(sum(34,2)).toBe(36);
 });
+
+const subtract = require('./subtract');
+
+test("testar a subtração", () => {
+    expect(subtract(10,4)).toBe(6);
+});
