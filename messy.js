e88077ec869700ce27d353acc0ea274dc1dea4bb
Diogo Ferreira <diogo.ferreira18@ua.pt>
2025-10-06 09:18:24 +0100

    test prettier run

--- messy.js
+++ messy.js
@@ -2,19 +2,15 @@ function messyFunction(a, b, c) {
   if (a > b) {
     return a + c;
   } else if (b > a) {
-    return b + c;
-  } else {
-    return c;
-  }
+    return b + c;} else {return c;}
 }
 
 const obj = {
   prop1: "hello",
   prop2: "world",
-  nested: {
-    x: 1,
-    y: 2,
-  },
+  nested: {x: 1,y: 2,},
 };
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
+const arr = [1, 2, 3, 4, 5, 
+  
+  6, 7];
